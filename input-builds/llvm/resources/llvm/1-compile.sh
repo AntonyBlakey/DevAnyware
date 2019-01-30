3fa8a3a460cc1247c656c18d3846e498a2e38b00
Antony Blakey <antony.blakey@gmail.com>
2019-01-30 09:21:21 +0100

    Remove github key :)

--- input-builds/llvm/resources/llvm/1-compile.sh
+++ input-builds/llvm/resources/llvm/1-compile.sh
@@ -2,9 +2,9 @@
 
 export BRANCH=release_80
 
-curl -s -L --insecure https://AntonyBlakey:bf8fbaf256b302a7be6582002b54bae08b75cf0d@codeload.github.com/llvm-mirror/llvm/tar.gz/${BRANCH} -o llvm-${BRANCH}.tar.gz
-curl -s -L --insecure https://AntonyBlakey:bf8fbaf256b302a7be6582002b54bae08b75cf0d@codeload.github.com/llvm-mirror/clang/tar.gz/${BRANCH} -o clang-${BRANCH}.tar.gz
-curl -s -L --insecure https://AntonyBlakey:bf8fbaf256b302a7be6582002b54bae08b75cf0d@codeload.github.com/llvm-mirror/lld/tar.gz/${BRANCH} -o lld-${BRANCH}.tar.gz
+curl -s -L --insecure https://codeload.github.com/llvm-mirror/llvm/tar.gz/${BRANCH} -o llvm-${BRANCH}.tar.gz
+curl -s -L --insecure https://codeload.github.com/llvm-mirror/clang/tar.gz/${BRANCH} -o clang-${BRANCH}.tar.gz
+curl -s -L --insecure https://codeload.github.com/llvm-mirror/lld/tar.gz/${BRANCH} -o lld-${BRANCH}.tar.gz
 
 mkdir /llvm/src
 tar -zxf llvm-${BRANCH}.tar.gz --strip-components=1 --directory=/llvm/src
