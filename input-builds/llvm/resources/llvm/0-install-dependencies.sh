bd786b1099ce588172594c93def82a396270d242
Antony Blakey <antony.blakey@gmail.com>
2019-01-30 12:47:19 +0100

    Switch to official llvm git & build process

--- input-builds/llvm/resources/llvm/0-install-dependencies.sh
+++ input-builds/llvm/resources/llvm/0-install-dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 apt-get update
-apt-get install -y --no-install-recommends cmake ninja-build clang python
+apt-get install -y --no-install-recommends cmake ninja-build clang swig python2.7-dev libedit-dev libncurses5-dev
--- input-builds/llvm/resources/llvm/1-compile.sh
+++ input-builds/llvm/resources/llvm/1-compile.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env zsh
 
-export BRANCH=release_80
-
-curl -s -L --insecure https://codeload.github.com/llvm-mirror/llvm/tar.gz/${BRANCH} -o llvm-${BRANCH}.tar.gz
-curl -s -L --insecure https://codeload.github.com/llvm-mirror/clang/tar.gz/${BRANCH} -o clang-${BRANCH}.tar.gz
-curl -s -L --insecure https://codeload.github.com/llvm-mirror/lld/tar.gz/${BRANCH} -o lld-${BRANCH}.tar.gz
-
-mkdir /llvm/src
-tar -zxf llvm-${BRANCH}.tar.gz --strip-components=1 --directory=/llvm/src
-mkdir /llvm/src/tools/clang
-tar -zxf clang-${BRANCH}.tar.gz --strip-components=1 --directory=/llvm/src/tools/clang
-mkdir /llvm/src/tools/lld
-tar -zxf lld-${BRANCH}.tar.gz --strip-components=1 --directory=/llvm/src/tools/lld
-rm *.tar.gz
-
-mkdir /llvm/build
-cd /llvm/build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/llvm -DLLVM_TARGETS_TO_BUILD=X86 /llvm/src
+git clone https://github.com/llvm/llvm-project.git --branch release/8.x --depth 1
+mkdir llvm-project/build
+pushd llvm-project/build
+cmake ../llvm -G Ninja \
+    -DLLVM_ENABLE_PROJECTS='clang;libcxx;libcxxabi;libunwind;lldb;compiler-rt;lld' \
+    -DCMAKE_INSTALL_PREFIX=/llvm \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_TARGETS_TO_BUILD=X86
 cmake --build . --target install
+popd
+rm -rf llvm-project
+
 ln -s /llvm/bin/clang /llvm/bin/cc
-rm -rf /llvm/src /llvm/build
