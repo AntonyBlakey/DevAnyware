0455443088e6f21637862c3eb08af044b55453c7
Antony Blakey <antony.blakey@gmail.com>
2019-02-03 23:03:11 +0100

    Start of commando UI

--- base/modules/120-x11/resources-home-dev/.config/xchainkeys/show_help.sh
+++ base/modules/120-x11/resources-home-dev/.config/xchainkeys/show_help.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env zsh
 
-DIR=/home/dev/.config/xchainkeys
-cat $DIR/help-*.txt > $DIR/help.txt
-st -t 'i3 command mode help' -g $(wc -L -l $DIR/help.txt | awk '{ print $2"x"($1+1) }') -e zsh -c "cat $DIR/help.txt && sleep 600"
+commando-show-image $(commando-html-to-png /home/dev/.config/xchainkeys/help-0.html)
