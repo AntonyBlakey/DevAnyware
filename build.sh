37d10ec42392fa21dba214d637fa7d8954d5e760
Antony Blakey <antony@soops.nl>
2019-10-28 18:34:48 +0100

    Refactoring for versioned builds

--- build.sh
+++ build.sh
@@ -10,16 +10,45 @@ for input in $inputs ; do
     (cd inputs/$input && ./build.sh)
 done
 
-docker_modules=(base x11 c python conan elixir go racket sbcl java haskell pandoc xmonad neovim javascript clojurescript rust)
-# docker_modules=(base x11 c python conan java haskell xmonad neovim javascript rust)
+version_major=1
+version=${version_major}.0.0
+
 previous_docker_module='ubuntu:19.04'
 
+# UI
+docker_modules=(base x11 haskell xmonad)
 for docker_module in $docker_modules ; do
-    name=devanyware/$docker_module
+    name=devanyware/module/$docker_module
     ./build-from-modules.sh $name $previous_docker_module $docker_module
     previous_docker_module=$name
 done
 
-docker tag $previous_docker_module devanyware/full
+docker tag ${previous_docker_module} devanyware/ui:${version}
+docker tag ${previous_docker_module} devanyware/ui:${version_major}
+docker tag ${previous_docker_module} devanyware/ui:latest
+
+# Minimal
+docker_modules=(c python conan javascript rust)
+for docker_module in $docker_modules ; do
+    name=devanyware/module/$docker_module
+    ./build-from-modules.sh $name $previous_docker_module $docker_module
+    previous_docker_module=$name
+done
+
+docker tag ${previous_docker_module} devanyware/minimal:${version}
+docker tag ${previous_docker_module} devanyware/minimal:${version_major}
+docker tag ${previous_docker_module} devanyware/minimal:latest
+
+# Full
+docker_modules=(java neovim elixir go racket sbcl pandoc clojurescript)
+for docker_module in $docker_modules ; do
+    name=devanyware/module/$docker_module
+    ./build-from-modules.sh $name $previous_docker_module $docker_module
+    previous_docker_module=$name
+done
+
+docker tag $previous_docker_module devanyware/full:${version}
+docker tag $previous_docker_module devanyware/full:${version_major}
+docker tag $previous_docker_module devanyware/full:latest
 
 echo "FINISH"
